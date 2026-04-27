d4a3ca38284b70d20ec27b8b01f20e690bd9c422
RidaA06 <rakhter1@live.maryville.edu>
2026-04-26 22:43:36 -0500

    Refactor user save function to include additional fields
    Updated user saving logic to include full name, password hash, and phone number. Changed JSON indentation from 4 to 2 spaces.

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,12 @@
 
+
 import json
 import os
 from models import User
 
 USER_FILE = 'users.json'
 MEAL_FILE = 'meals.json'
+
 
 def load_users():
     if not os.path.exists(USER_FILE):
@@ -15,52 +17,45 @@ def load_users():
     users = {}
     for username, info in data.items():
         users[username] = User(
-            id=info["id"], 
-            name=info["name"], 
-            role=info["role"], 
+            id=username,
+            name=info["full_name"],   
+            role=info["role"],
             active=info["active"]
         )
     return users
 
-def save_user(new_user):
-    users = load_users()
-    data = {}
-    
 
-    for u_id, uobj in users.items():
-        data[u_id] = {
-            "id": uobj.id,
-            "name": uobj.name,
-            "role": uobj.role,
-            "active": uobj.active
-        }
-        
+def save_user(new_user, password_hash="", phone=""):
+    if not os.path.exists(USER_FILE):
+        data = {}
+    else:
+        with open(USER_FILE, 'r') as file:
+            data = json.load(file)
+
+    
     data[new_user.id] = {
-        "id": new_user.id,
-        "name": new_user.name,
+        "full_name": new_user.name,
+        "phone": phone,
+        "password_hash": password_hash,
         "role": new_user.role,
         "active": new_user.active
     }
-    
-  
+
     with open(USER_FILE, 'w') as file:
-        json.dump(data, file, indent=4)
+        json.dump(data, file, indent=2)
+
 
 def load_meals():
     if not os.path.exists(MEAL_FILE):
-        return [] 
-        
+        return []
     with open(MEAL_FILE, 'r') as file:
         return json.load(file)
+
 
 def save_meal(new_meal):
     meals = load_meals()
     meals.append(new_meal)
-    
-    with open(MEAL_FILE, "w") as file:
-        json.dump(meals, file, indent=4)
-        
-
-    ## import data management 
+    with open(MEAL_FILE, 'w') as file:
+        json.dump(meals, file, indent=2)
     
  
